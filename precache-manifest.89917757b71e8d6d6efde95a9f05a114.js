878ebba0bab733772de6860aa0072ff3aec3075d
MoAqsam <MoChainz@localhost.com>
2019-02-22 21:35:43 -0500

    commit init

---
+++ precache-manifest.89917757b71e8d6d6efde95a9f05a114.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f44d315ba36469f19dcf",
+    "url": "/static/js/main.f44d315b.chunk.js"
+  },
+  {
+    "revision": "d85ceb0d723314090ff1",
+    "url": "/static/js/2.d85ceb0d.chunk.js"
+  },
+  {
+    "revision": "f44d315ba36469f19dcf",
+    "url": "/static/css/main.df596968.chunk.css"
+  },
+  {
+    "revision": "bbd1b251c888acabe51fbcdb97b8f28f",
+    "url": "/index.html"
+  }
+];
