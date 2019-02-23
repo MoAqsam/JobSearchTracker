c0add25bd797af805d26277a2967c46f7d66d72c
MoAqsam <MoChainz@localhost.com>
2019-02-22 21:52:41 -0500

    new changes and build scripts

---
+++ precache-manifest.ffee2702ebf28ec95c0bf95d47b4d54a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "385a88e9b0ee8381da47",
+    "url": "/JobSearchTracker/static/js/runtime~main.385a88e9.js"
+  },
+  {
+    "revision": "3af340cf9dfa4ffb8790",
+    "url": "/JobSearchTracker/static/js/main.3af340cf.chunk.js"
+  },
+  {
+    "revision": "d85ceb0d723314090ff1",
+    "url": "/JobSearchTracker/static/js/2.d85ceb0d.chunk.js"
+  },
+  {
+    "revision": "3af340cf9dfa4ffb8790",
+    "url": "/JobSearchTracker/static/css/main.df596968.chunk.css"
+  },
+  {
+    "revision": "23277576125bfd4a62224cbbc1932777",
+    "url": "/JobSearchTracker/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.89917757b71e8d6d6efde95a9f05a114.js"
+  "/JobSearchTracker/precache-manifest.ffee2702ebf28ec95c0bf95d47b4d54a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/JobSearchTracker/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
