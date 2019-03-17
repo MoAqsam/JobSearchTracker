3bf9a6ce84df36cebc9674df777a18410c0f00bd
MoAqsam <MoChainz@localhost.com>
2019-03-16 21:52:11 -0400

    added edit feature -- incomplete

--- src/MainContainer/MainContainer.js
+++ src/MainContainer/MainContainer.js
@@ -4,26 +4,20 @@ import uuid from "uuid";
 
 class MainContainer extends Component {
 
-    componentDidMount() {
-        // localStorage.removeItem("aqsam_jobs")
-
-        console.log(JSON.parse(localStorage.getItem("aqsam_jobs")))
-    }
-
     onChangeModal(evt) {
         this.setState({
             [evt.target.name]: evt.target.value
         });
     }
 
-    delete(id){
-       let jobs = JSON.parse(localStorage.getItem("aqsam_jobs"));
-        jobs = jobs.filter((data)=>{
-          return (data.id !== id)
+    delete(id) {
+        let jobs = JSON.parse(localStorage.getItem("aqsam_jobs"));
+        jobs = jobs.filter((data) => {
+            return (data.id !== id)
         });
-        localStorage.setItem("aqsam_jobs",JSON.stringify(jobs));
+        localStorage.setItem("aqsam_jobs", JSON.stringify(jobs));
         this.forceUpdate();
-    } 
+    }
 
     onSaveModal(evt) {
         var _Jobs = JSON.parse(localStorage.getItem("aqsam_jobs"));
@@ -40,10 +34,15 @@ class MainContainer extends Component {
         this.forceUpdate();
     }
 
+    onUpdateModal(evt) {
+        var _Jobs = JSON.parse(localStorage.getItem("aqsam_jobs"));
+        // unique id    
+    }
+
     mapJobs() {
         let jobs = JSON.parse(localStorage.getItem("aqsam_jobs"));
         // Cards
-        return (jobs.map( (data, i) => {
+        return (jobs.map((data, i) => {
             return (
                 <div key={i} className="col-sm-12 col-md-4">
                     <div
@@ -52,23 +51,62 @@ class MainContainer extends Component {
                         textAlign: "center",
                         margin: "10px 0px",
                         minHeight: "300px",
-                        width:"100%"
+                        width: "100%"
                     }}>
                         <div className="card-body">
+                            <button
+                                className="btn edit-button"                                
+                                data-target={"#"+data.companyName}
+                                data-toggle="modal"
+                                >Edit</button>
+                            <br/>
                             <div className="title">
-                                <h2 style={{color:"#17a2b8"}}>{data.companyName}</h2>
+                                <h2
+                                    style={{
+                                    color: "#17a2b8"
+                                }}>{data.companyName}</h2>
                             </div>
                             <div className="card-text">
                                 <h4>{data.jobTitle}</h4>
-                                <h4>{data.jobUrl}</h4>
+                                <h4>
+                                    <a href={data.jobUrl} target="_blank" rel="noopener noreferrer">{data.jobUrl}</a>
+                                </h4>
                                 <h4>{data.appDate}</h4>
                                 <h4>{data.contact}</h4>
                                 <h4>{data.notes}</h4>
                             </div>
                         </div>
-                        <button className="btn btn-danger" onClick={this.delete.bind(this,data.id)}>Delete</button>
+                        <button
+                            className="btn btn-danger"
+                            onClick={this
+                            .delete
+                            .bind(this, data.id)}
+                            style={{
+                            width: "100%"
+                        }}>Delete</button>
                     </div>
                 </div>
+
+            )
+        }));
+    }
+
+    mapToModals() {
+        let jobs = JSON.parse(localStorage.getItem("aqsam_jobs"));
+        // Cards
+        return (jobs.map((data, i) => {
+            return (
+                
+                    <BootstrapModals key={i}
+                        onChangeModal={this
+                        .onChangeModal
+                        .bind(this)}
+                        onUpdateModal={this
+                        .onUpdateModal
+                        .bind(this,data.id)}
+                        data={data}
+                        type="update"/>
+               
             )
         }));
     }
@@ -91,20 +129,22 @@ class MainContainer extends Component {
                     </div>
                 </div>
                 {/* Modal */}
+                {this.mapToModals()}
                 <BootstrapModals
                     onChangeModal={this
                     .onChangeModal
                     .bind(this)}
                     onSaveModal={this
                     .onSaveModal
-                    .bind(this)}/> {/* Render content area */}
+                    .bind(this)}
+                    type="add"/> {/* Render content area */}
                 <div className="container">
                     <div className="row">
-
-                        {localStorage.getItem("aqsam_jobs") === null || JSON.parse(localStorage.getItem("aqsam_jobs")).length === 0
+                        {localStorage.getItem("aqsam_jobs") === null || JSON
+                            .parse(localStorage.getItem("aqsam_jobs"))
+                            .length === 0
                             ? <h1>Lets get the job search going</h1>
                             : this.mapJobs()}
-
                     </div>
                 </div>
             </div>
--- src/components/bootstrapModals/BootstrapModals.js
+++ src/components/bootstrapModals/BootstrapModals.js
@@ -1,19 +1,21 @@
 import React from 'react';
 
 const BootstrapModals = (props) => {
-    console.log(props);
     return (
-        <div className="modal fade"  id="appModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal fade"  id={props.type === "add" ? "appModal" : props.data.companyName} tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div className="modal-dialog" role="document">
                 <div className="modal-content">
                     <div className="modal-header">
-                        <h5 className="modal-title">New application</h5>
+                        <h5 className="modal-title">{props.type === "add" ? "New Application" : "Update"}</h5>
                         <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
                     <div className="modal-body">
                     {/* form content goes here */}
+                    {
+                        props.type === "add" 
+                        ?
                         <form>
                             <div className="form-group">
                                 <label htmlFor="companyName">Company Name</label>
@@ -25,11 +27,11 @@ const BootstrapModals = (props) => {
                             </div>
                             <div className="form-group">
                                 <label htmlFor="jobUrl">Job Url</label>
-                                <input type="text" onChange={props.onChangeModal} name="jobUrl" className="form-control"/>
+                                <input type="text" onChange={props.onChangeModal} name="jobUrl" defaultValue="https://" className="form-control"/>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="appDate">Application Date</label>
-                                <input type="text" onChange={props.onChangeModal} name="appDate" className="form-control"/>
+                                <input type="text" onChange={props.onChangeModal} name="appDate"  className="form-control"/>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="contact">Contact</label>
@@ -39,10 +41,39 @@ const BootstrapModals = (props) => {
                                 <label htmlFor="notes">Notes</label>
                                 <textarea type="text" onChange={props.onChangeModal} name="notes" className="form-control"/>
                             </div>
-                        </form>
+                        </form>    
+                        :
+                        <form>
+                            <div className="form-group">
+                                <label htmlFor="companyName">Company Name</label>
+                                <input type="text" onChange={props.onChangeModal} name="companyName" defaultValue={props.data.companyName} className="form-control"/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="jobTitle">Job Title</label>
+                                <input type="text" onChange={props.onChangeModal} name="jobTitle" defaultValue={props.data.jobTitle} className="form-control"/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="jobUrl">Job Url</label>
+                                <input type="text" onChange={props.onChangeModal} name="jobUrl" defaultValue={props.data.jobUrl} className="form-control"/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="appDate">Application Date</label>
+                                <input type="text" onChange={props.onChangeModal} name="appDate" defaultValue={props.data.appDate} className="form-control"/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="contact">Contact</label>
+                                <input type="text" onChange={props.onChangeModal} name="contact" defaultValue={props.data.contact} className="form-control"/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="notes">Notes</label>
+                                <textarea type="text" onChange={props.onChangeModal}  name="notes" defaultValue={props.data.notes} className="form-control"/>
+                            </div>
+                        </form> 
+                    }
+                        
                     </div>
                     <div className="modal-footer">
-                        <button type="button" className="btn btn-info" data-dismiss="modal" onClick={props.onSaveModal}>Save changes</button>
+                        <button type="submit" className="btn btn-info" data-dismiss="modal" onClick={props.type === "add" ? props.onSaveModal  : props.onUpdateModal}   >{props.type === "add" ? "Save changes" : "Update changes" }</button>
                         <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
                     </div>
                 </div>
@@ -51,5 +82,6 @@ const BootstrapModals = (props) => {
 
     );
 }
+
 
 export default BootstrapModals;
