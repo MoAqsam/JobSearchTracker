a2abda3ccaa0baeb8e537f4860f6bead9b7efe99
MoAqsam <MoChainz@localhost.com>
2019-03-19 20:01:49 -0400

    adding loading screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React, { Component} from "react";
+import React, { Component, Fragment} from "react";
 import MainContainer from './MainContainer/MainContainer';
 
 import "./App.css";
+import LoadingScreen from './components/loadingScreen/LoadingScreen';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoading: false
+    }
+  }
+  componentDidMount(){
+    console.log("update")
+    setTimeout(() => {
+      this.setState({
+        isLoading : true
+      });
+    }, 1200);
+  }
+
   render(){
     return(
-      <MainContainer/>
+      <Fragment>
+        {this.state.isLoading === true ?  <MainContainer/> : <LoadingScreen/>}       
+      </Fragment>
     )
   };
 }
